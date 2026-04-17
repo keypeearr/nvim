2d1b2472d17b34758bae83179b4404dd8e2cf717
kylerequez <kyle.requez.code@gmail.com>
2026-04-17 10:37:39 +0800

    Removed Treesitter config

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -36,12 +36,27 @@ vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
 
+vim.o.confirm = true
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
+})
+
+vim.diagnostic.config({
+	update_in_insert = false,
+	severity_sort = true,
+	float = { border = "rounded", source = "if_many" },
+	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+	-- Can switch between these as you prefer
+	virtual_text = true, -- Text shows up at the end of the line
+	virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+	jump = { float = true },
 })
 
 vim.opt.tabstop = 4
